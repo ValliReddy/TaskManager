cce9b8828b9cf320627d11f2dc6edf7a23afac2d
ValliReddy <157823804+ValliReddy@users.noreply.github.com>
2024-03-02 02:28:20 -0800

    added authentication

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for,session
+from models import db,Task,User
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task_manager_new.db'
+app.secret_key = "secret_keying"
+db.init_app(app)
+
 task_list=[]
 @app.route("/",methods=["GET","POST"])
 def Home():
@@ -9,11 +14,30 @@ def Home():
 
 @app.route("/login",methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        user = User.query.filter_by(name=name).first()
 
+        if user and user.check_password(password):
+            session["name"] = user.email
+            return redirect("/task")
+        else:
+            return render_template("login.html", error="Invalid user or password")
+    return render_template("login.html")
 @app.route("/signup",methods=["GET","POST"])
 def signup():
-    return render_template("signup.html")
+  if request.method == "POST":
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+
+    new_user = User(name=name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/login")
+  return render_template("signup.html")
+
 
 @app.route("/task",methods=["GET","POST"])
 def task():
@@ -32,5 +56,11 @@ def fill():
 
 
 
+
+
 if __name__=="__main__":
+    with app.app_context():
+        # db.drop_all()
+        db.create_all()
+
     app.run(debug=True)
